d2820f04945435f965bb685c70a381f031ac7280
Kirill Shakirov <kirillshakirov57@gmail.com>
2022-12-08 11:58:25 +0400

    failed vaa
    no-working foreign asset solana
    
    working first part of th transfer
    
    failed to fetch VAA: not found in store
    
    get account info error
    
    task: broken maiinet transfer
    
    failing solana transaction
    
    task: working transfer

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,43 @@
-export const ETH_TOKEN_BRIDGE_ADDRESS =
-  "0x0290FB167208Af455bB137780163b7B7a9a10C16";
+// tokens
+export const USDT_ERC20_MAINNET = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+export const WETH_TESTNET = "0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6";
+export const WETH_MAINNET = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+export const TEST_ERC20_TESTNET = "0x2D8BE6BF0baA74e0A907016679CaE9190e80dD0A"; // @QUESTION WHAT is This token address
+export const TEST_ERC20_MAINNET = "0xdAC17F958D2ee523a2206206994597C13D831ec7"; // THIS IS USDT ERC20
 
-export const ETH_BRIDGE_ADDRESS = "0xC89Ce4735882C9F0f0FE26686c53074E09B0D550";
+// wormhole contracts mainnet
+export const SOLANA_TOKEN_BRIDGE_MAINNET =
+  "wormDTUJ6AWPNvk59vGQbDvGJmqbDTdgWgAqcLBCgUb";
+export const SOLANA_CORE_BRIDGE_MAINNET =
+  "worm2ZoG2kUd4vFXhvjh93UUH596ayRfgQ2MgjNMTth";
+export const ETH_TOKEN_BRIDGE_MAINNET =
+  "0x3ee18B2214AFF97000D974cf647E7C347E8fa585";
+export const ETH_CORE_BRIDGE_MAINNET =
+  "0x98f3c9e6E3fAce36bAAd05FE09d375Ef1464288B";
 
+// wormhole contracts testnet
+export const SOLANA_TOKEN_BRIDGE_DEVNET =
+  "DZnkkTmCiFWfYTfT41X3Rd1kDgozqzxWaHqsw6W4x2oe";
+export const SOLANA_CORE_BRIDGE_DEVNET =
+  "Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o";
+export const ETH_TOKEN_BRIDGE_TESTNET =
+  "0xF890982f9310df57d00f659cf4fd87e65adEd8d7";
+export const ETH_CORE_BRIDGE_TESTNENT =
+  "0x706abc4E45D419950511e474C7B9Ed348A4a716c";
+
+// keys mainnet
 export const ETH_PRIVATE_KEY =
-  "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d";
-export const ETH_NODE_URL =
-  "wss://evocative-indulgent-friday.ethereum-goerli.discover.quiknode.pro/bf63660f3a2180b857109ba4cdf8da59bce59b27/";
-export const WORMHOLE_RPC_HOST = "https://www.quicknode.com/endpoints";
-export const TEST_ERC20 = "0x2D8BE6BF0baA74e0A907016679CaE9190e80dD0A";
-// export const TEST_ERC20 = "0x7af963cF6D228E564e2A0aA0DdBF06210B38615D";
+  "b3bd074f50513bc4dc27fd336c2b24b88f09e3ff973970159ad30c79139a25e8";
+export const SOL_PRIVATE_KEY =
+  "5riJzLz1EMUsogENXnWrRyDURSHhfzbAUaJnnxwvYZ43Nhvmb6XMosKDpvrpjSYN5M5rgdR8SKoVGWDVSXLM1Rxz";
+
+// rpc endpoints
+export const WORMHOLE_RPC_HOST_MAINNET = "https://wormhole.inotel.ro";
+export const WORMHOLE_RPC_HOST_TESTNET =
+  "https://wormhole-v2-testnet-api.certus.one";
+export const ETH_NODE_URL_MAINNET =
+  "wss://chaotic-hidden-shape.discover.quiknode.pro/5425bf89f1b0d6b649a3a08e6c1d422c07a875be/";
+export const ETH_NODE_URL_TESTNET =
+  "wss://bold-spring-shape.ethereum-goerli.discover.quiknode.pro/552834c379cccc468688e95496b9f3565ba1954d/";
+export const SOLANA_NODE_URL_MAINNET =
+  "https://p2p.rpcpool.com/82313b15169cb10f3ff230febb8d";
---
+++ src/utils.ts
@@ -0,0 +1,189 @@
+import {
+  ETH_CORE_BRIDGE_MAINNET,
+  ETH_NODE_URL_MAINNET,
+  ETH_PRIVATE_KEY,
+  ETH_TOKEN_BRIDGE_MAINNET,
+  SOLANA_CORE_BRIDGE_MAINNET,
+  SOLANA_TOKEN_BRIDGE_MAINNET,
+  WETH_MAINNET,
+  WORMHOLE_RPC_HOST_MAINNET,
+} from "./constants";
+import {
+  approveEth,
+  CHAIN_ID_ETH,
+  CHAIN_ID_SOLANA,
+  getEmitterAddressEth,
+  getForeignAssetSolana,
+  getIsTransferCompletedSolana,
+  getOriginalAssetEth,
+  getSignedVAAWithRetry,
+  parseSequenceFromLogEth,
+  redeemOnSolana,
+  transferFromEthNative,
+  tryNativeToUint8Array,
+} from "@certusone/wormhole-sdk";
+import { postVaaWithRetry } from "@certusone/wormhole-sdk/lib/cjs/solana/sendAndConfirmPostVaa";
+import { Connection, Keypair, PublicKey, Transaction } from "@solana/web3.js";
+import { ethers } from "ethers";
+import {
+  createAssociatedTokenAccountInstruction,
+  getAssociatedTokenAddress,
+} from "@solana/spl-token";
+import { parseUnits } from "@ethersproject/units";
+
+export const getSequence = async (
+  connection: Connection,
+  solKeypair: Keypair
+) => {
+  const provider = new ethers.providers.WebSocketProvider(ETH_NODE_URL_MAINNET);
+
+  const ethSigner = new ethers.Wallet(ETH_PRIVATE_KEY, provider);
+
+  const originalAsset = await getOriginalAssetEth(
+    ETH_TOKEN_BRIDGE_MAINNET,
+    provider,
+    WETH_MAINNET,
+    CHAIN_ID_ETH
+  );
+
+  console.log(originalAsset);
+
+  // @QUESTION should it be solana address of wrapped wormhole token (so we hardcoded this from the portal bridge)
+  let solanaMint = await getForeignAssetSolana(
+    connection,
+    SOLANA_TOKEN_BRIDGE_MAINNET,
+    CHAIN_ID_ETH,
+    originalAsset.assetAddress // Taken from the bridge UI
+  );
+
+  solanaMint = "7vfCXTUXx5WJV5JADk17DUJ4ksgau7utNKj4b963voxs";
+  console.log(solanaMint);
+
+  if (!solanaMint) {
+    throw new Error("No Solana mint");
+  }
+
+  const solanaMintPubKey = new PublicKey(solanaMint);
+
+  const recipient = await getAssociatedTokenAddress(
+    solanaMintPubKey,
+    solKeypair.publicKey
+  );
+
+  console.log("receipt", recipient);
+
+  const associatedAddressInfo = await connection.getAccountInfo(recipient);
+
+  console.log("ass token info", associatedAddressInfo);
+
+  if (!associatedAddressInfo) {
+    console.log("creating associated token account");
+    const transaction = new Transaction().add(
+      await createAssociatedTokenAccountInstruction(
+        solKeypair.publicKey, // payer
+        recipient,
+        solKeypair.publicKey, // owner
+        solanaMintPubKey
+      )
+    );
+    const { blockhash } = await connection.getLatestBlockhash();
+    transaction.recentBlockhash = blockhash;
+    transaction.feePayer = solKeypair.publicKey;
+    // sign, send, and confirm transaction
+    transaction.partialSign(solKeypair);
+    const txid = await connection.sendRawTransaction(transaction.serialize());
+    const confirmedTransaction = await connection.confirmTransaction(txid);
+    console.log("Transaction  confirmation", confirmedTransaction);
+  }
+  const amount = parseUnits("1", 1);
+  console.log("Amount to send", amount);
+  // approve the bridge to spend tokens
+  await approveEth(ETH_TOKEN_BRIDGE_MAINNET, WETH_MAINNET, ethSigner, amount);
+  // // transfer tokens
+  const receipt = await transferFromEthNative(
+    ETH_TOKEN_BRIDGE_MAINNET,
+    ethSigner,
+    amount,
+    CHAIN_ID_SOLANA,
+    tryNativeToUint8Array(recipient.toString(), CHAIN_ID_SOLANA)
+  );
+
+  const sequence = parseSequenceFromLogEth(receipt, ETH_CORE_BRIDGE_MAINNET);
+
+  console.log("got sequence", sequence);
+
+  return sequence;
+};
+
+export const finilizeTransfer = async (
+  sequence: string,
+  connection: Connection,
+  solKeyPair: Keypair
+) => {
+  console.log("trying get VAA");
+  const emitterAddress = getEmitterAddressEth(ETH_TOKEN_BRIDGE_MAINNET);
+
+  console.log("emitter address", emitterAddress);
+
+  const { vaaBytes: signedVAA } = await getSignedVAAWithRetry(
+    [WORMHOLE_RPC_HOST_MAINNET],
+    CHAIN_ID_ETH,
+    "0000000000000000000000003ee18b2214aff97000d974cf647e7c347e8fa585",
+    "96102"
+    // emitterAddress,
+    // sequence
+  );
+
+  console.log("SIGNEDVAA");
+  console.log(signedVAA);
+
+  let maxFailures = 1;
+
+  const postPromise = postVaaWithRetry(
+    connection,
+    async (transaction: Transaction) => {
+      console.log("Signing transaction");
+      transaction.partialSign(solKeyPair);
+      console.log("Signed  transaction");
+      return transaction;
+    },
+    SOLANA_CORE_BRIDGE_MAINNET,
+    solKeyPair.publicKey.toString(),
+    Buffer.from(signedVAA),
+    maxFailures
+  );
+
+  const signature = await postPromise;
+
+  console.log("signature");
+  console.log(signature);
+
+  // redeem tokens on solana
+  console.log("awaited post promise");
+  const transaction = await redeemOnSolana(
+    connection,
+    SOLANA_CORE_BRIDGE_MAINNET,
+    SOLANA_TOKEN_BRIDGE_MAINNET,
+    solKeyPair.publicKey.toString(),
+    signedVAA
+  );
+  // sign, send, and confirm transaction
+  console.log("got redeemed on solana tx");
+  transaction.partialSign(solKeyPair);
+  console.log("partially signed redeem tx");
+
+  // @FIXME fails here
+  const txid = await connection.sendRawTransaction(transaction.serialize());
+  console.log("sent raw tx");
+
+  await connection.confirmTransaction(txid);
+  console.log("confirmed");
+
+  const confirmed = await getIsTransferCompletedSolana(
+    SOLANA_TOKEN_BRIDGE_MAINNET,
+    signedVAA,
+    connection
+  );
+
+  console.log("CONFIRMED:", confirmed);
+};
